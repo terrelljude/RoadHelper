c536f586468d957b060beb491289c4041e0bd4b4
Terrell-Jude Ilechie <terrelljudei@fb.com>
2020-08-07 12:09:32 -0400

    update

--- RoadHelper/ViewController.m
+++ RoadHelper/ViewController.m
@@ -10,13 +10,58 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITableView *tableview;
+@property (nonatomic, strong) NSArray *gasstations;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.tableview.dataSource = self;
+    self.tableview.delegate = self;
     // Do any additional setup after loading the view.
+    
+NSURL *url = [NSURL URLWithString:@"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=39.000990,-76.769780&radius=1000&type=gasstation&keyword=car&key=AIzaSyAt9E5urPXSm4eXcIt97PJl-8NGRo7aiq4"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               
+               NSLog (@"%@", dataDictionary);
+               
+               self.gasstations = dataDictionary[@"results"];
+               for (NSDictionary *gasstation in self.gasstations) {
+                   NSLog(@"%@", gasstation[@"business_status"]);
+               }
+
+           
+               
+           
+           }
+       }];
+    [task resume];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Configure the view for the selected state
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *) tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
 }
 
 
